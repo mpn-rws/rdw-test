60544ced3a2ed9934490b72d642f8a44260ca69b
mpn-rws <119926234+mpn-rws@users.noreply.github.com>
2022-12-06 09:56:20 +0100

    Update app_rdw.py

--- app_rdw.py
+++ app_rdw.py
@@ -10,8 +10,6 @@ st.title("Real-Time / Live Data Science Dashboard")
 @st.cache(allow_output_mutation=True)
 def load_data():    
     df_imp = pd.read_csv('https://opendata.rdw.nl/resource/8ys7-d773.csv', delimiter = ';')
-    df_imp = df_imp[df_imp.ASDRUK1 > 0]
-    df_imp = df_imp[df_imp.ASDRUK2 > 0] 
     df_imp = df_imp.fillna(0)
     df_imp = df_imp.reset_index(drop=True)
     return df_imp
