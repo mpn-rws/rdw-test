2e25fab82f3ce3579bc4a48ceab22fc7f6d97db3
mpn-rws <119926234+mpn-rws@users.noreply.github.com>
2022-12-06 10:20:53 +0100

    Update app_rdw.py

--- app_rdw.py
+++ app_rdw.py
@@ -10,7 +10,7 @@ pd.set_option('display.max_rows', None)
 
 @st.cache(allow_output_mutation=True)
 def load_data():    
-    df_imp = pd.read_json('https://opendata.rdw.nl/resource/m9d7-ebf2.json',nrows=None)
+    df_imp = pd.read_json('https://opendata.rdw.nl/resource/m9d7-ebf2.json',lines=True)
     df_imp = df_imp.fillna(0)
     df_imp = df_imp.reset_index(drop=True)
     return df_imp
