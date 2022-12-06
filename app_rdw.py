f79a5cedc2018baba437b47f89aec2ef6cff81c5
mpn-rws <119926234+mpn-rws@users.noreply.github.com>
2022-12-06 10:09:35 +0100

    Update app_rdw.py

--- app_rdw.py
+++ app_rdw.py
@@ -9,7 +9,7 @@ st.title("Real-Time / Live Data Science Dashboard")
 
 @st.cache(allow_output_mutation=True)
 def load_data():    
-    df_imp = pd.read_json('https://opendata.rdw.nl/resource/m9d7-ebf2.json?$limit=100000000')
+    df_imp = pd.read_json('https://opendata.rdw.nl/resource/m9d7-ebf2.json?$limit=1000')
     df_imp = df_imp.fillna(0)
     df_imp = df_imp.reset_index(drop=True)
     return df_imp
