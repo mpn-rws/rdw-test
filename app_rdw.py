8d74edab457c0f329d44d9b5935c36e8ff0e071c
mpn-rws <119926234+mpn-rws@users.noreply.github.com>
2022-12-06 09:53:13 +0100

    Add files via upload

---
+++ app_rdw.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+
+# dashboard title
+st.title("Real-Time / Live Data Science Dashboard")
+
+
+@st.cache(allow_output_mutation=True)
+def load_data():    
+    df_imp = pd.read_csv('https://opendata.rdw.nl/resource/8ys7-d773.csv', delimiter = ';')
+    df_imp = df_imp[df_imp.ASDRUK1 > 0]
+    df_imp = df_imp[df_imp.ASDRUK2 > 0] 
+    df_imp = df_imp.fillna(0)
+    df_imp = df_imp.reset_index(drop=True)
+    return df_imp
+
+df = load_data() 
+
+st.dataframe(df)
